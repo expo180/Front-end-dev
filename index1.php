0c686e64acc851cfe434fb2cfe26dfc6ad26fbb4
expo180 <badersaadousalissou@gmail.com>
2023-03-04 13:35:53 +0100

    Here you will find the main php code for Frame Genesis main page

---
+++ index1.php
@@ -0,0 +1,310 @@
+<!DOCTYPE html>
+<html lang="FR">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Accueil</title>
+	<link rel="stylesheet" type="text/css" href="mainstyles.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Gajraj+One&family=Gloock&family=Inter:wght@100;200;300;900&family=Open+Sans:wght@300&family=Poppins:wght@400;500;900&family=Questrial&family=Roboto:ital,wght@0,900;1,700&family=Source+Serif+Pro:ital,wght@1,600&family=Urbanist:wght@800&display=swap" rel="stylesheet">
+	<script src="main-script.js"></script>
+</head>
+<body>
+	<main>
+		<?php include("menu.php"); ?>
+	<section class="main-section">
+		<section  id="Accueil" class="section-one">
+			<div class="section-one-paragraph">
+				<div>
+					<h1 id="first-heading" class="number-one">La <strong class="science">science:</strong> <strong class="science">quête</strong> de <strong class="science">l'immortalité</strong> et de la <strong class="science">sagesse</strong></h1>
+				</div>
+				<div class="section-one-class">
+					<p class="first-paragraph">
+						La <strong class="science">science</strong> consiste à explorer l'inconnu, elle ne consiste pas à mémoriser un ensemble de faits dont on ne comprend pas le sens. Dans le passé, les scientifiques étaient considérés comme des <strong class="science">dieux</strong> car eux seuls parvenaient à comprendre la <strong class="science">nature et ses mystères</strong>. A l'ère de <strong class="science"> l'alchimie les savants de l'époque avaient pour seul but de trouver le fameux remède contre la mort</strong>. Quête qui ne finit point par aboutir. Cependant la science conntinue de servir l'homme. Elle lui permit de construire des satellites, des avions de chasse supersonic, de réaliser la reproduction In Vitro. Ici vous découvrirez des cours de science et d'ingénierie comme vous ne les avez jamais faits par le passé.
+					</p>
+				</div>
+				<div class="call-to">
+					<a href="" id="inscrip">S'inscrire</a>
+					<a href="" id="sav">En savoir plus</a>
+				</div>
+			</div>
+			<div>
+				<img src="./media_cyberhint/ai (1).png" class="hero-img" loading="lazy">
+			</div>
+		</section>
+		<section id="cours" class="section-two">
+			<h3 id="main-head" class="Topics-header">Nos Cours et Formations</h3>
+			<div class="courses-container">
+				<div class="python-courses">
+					<div class="courses-desc">
+						<img src="./media_cyberhint/python (1).png" class="courses-icons" loading="lazy">
+						<h3 class="courses-head" id="un">Conception d'applications Mobiles et Desktop avec le langage Python</h3> 
+					</div>
+					<div class="main-courses">
+						<p class="text-desc">
+							Le language <strong>Python</strong> est l'un des langages de progammmation les plus polulaires au monde vu <strong>sa simplicité</strong> et sa <strong>versatilité,</strong> Python est le langage le plus aimé par les programmeurs. Dans ce cours <strong>nous</strong> vous aiderons à maîtriser <strong>les bases de la programmation en Python</strong> jusqu'aux notions avancées telles que <strong>l'intelligence artificielle, les sciences de données, l'apprentissage machine</strong> et aussi le <strong>dévelopement d'applications Android et Desktop</strong>.
+						</p>
+					</div>
+				</div>
+				<div class="unity-courses">
+					<div class="courses-desc">
+						<img src="./media_cyberhint/unity.png" class="courses-icons" loading="lazy">
+						<h3 class="courses-head" id="deux">Développement de jeux vidéos avec le moteur Unity<sup>&reg</sup></h3>
+					</div>
+					<div class="main-courses">
+						<p class="text-desc">
+							Le <strong>game design</strong> est le domaine le plus excitants de la tech. Ici nous vous proposons une <strong>introduction gratuite</strong>sur <strong>le développement de jeux vidéos avec l'un des meilleurs moteurs de développement:</strong> le moteur Unity. Vous verrez que le développement de <strong>jeux vidéos</strong> n'est pas aussi amusant que le jeu lui même. En effet de bases solid en géométrie analytique et en logique sont nécessaires tout au cours tout <strong>au cours du développement</strong>.						
+						</p>
+					</div>
+				</div>
+				<div class="machine-courses">
+					<div class="courses-desc">
+						<img src="./media_cyberhint/excel-file.png" class="courses-icons" loading="lazy">
+						<h3 class="courses-head" id="trois">Analyse et visualisation de donées avec Microsoft Excel<sup id="excel">&reg</sup></h3>
+					</div>
+					<div class="main-courses">
+						<p class="text-desc">
+							L' <strong>analyse de données</strong> est essentielle dans <strong>l'optimisation de la production et la gestion des ressources au sein d'une entreprise</strong>. Les outils de calcul de <strong>Microsoft Excel</strong> permettent aux <strong>analystes et directeurs marketing</strong> de faire des prédictions <strong>rapidement</strong> et sans <strong>aucune erreur</strong>. En cette époque savoir utiliser les <strong>outils de développement</strong> proposés par <strong>Excel</strong> est plus que nécessaire. 
+						</p>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="A propos" class="section-three">
+			<div class="why-us">
+				<h3 id=" why-us-head">Pourquoi <strong>nous ?</strong></h3>
+			</div>
+			<div>
+				<div class="why-us-explanatory first">
+					Nous sommes une équipe <strong class="game">de passionnés</strong> 
+					dont le but principal est de<strong class="game"> préserver la science et ses valeurs telles que la créativité, la curiosité et surtout la recherche désintéressée du savoir.</strong>. La science est le début de la sagesse, elle donne du sens à notre existence.<strong class="game"> Sans science pas de développement.</strong>.
+				</div>
+				<div class="description">
+					<div class="why-us-column">
+						<div class="numero">1</div>
+						<div class="why-us-main">
+							<h2 class="why-us-steps-heading">Créativité</h2>
+							<p class="text-desc why">
+								Notre <strong class="game">façon d'enseigner</strong> les concepts se base sur les dernières <strong class="game">
+								recherches en neuroscience</strong> qui disent que la <strong class="game">meilleure façon d'aider 
+								le cerveau</strong> à retenir des <strong class="game">concepts abstraits</strong> 
+								est de les utiliser dans des exemples concrets. De cet fait, <strong class="game">toutes nos
+								</strong>formations sont essentiellement pratiques.
+							</p>
+						</div>
+					</div>
+					<div class="why-us-column">
+						<div class="numero">2</div>
+						<div class="why-us-main">
+							<h2 class="why-us-steps-heading">Focus</h2>
+							<p class="text-desc why">
+								Frame Genesis est une <strong class="game">institution orientée objet</strong>, 
+								c'est à dire que au fur et à mesure que vous suivez <strong class="game">nos formations nous nous 
+								assurerons</strong> que vous êtes bien à l'aise et que votre apprentissage se poursuit de manière harmonieuse. 
+							</p>
+						</div>
+					</div>
+					<div class="why-us-column">
+						<div class="numero">3</div>
+						<div class="why-us-main">
+							<h2 class="why-us-steps-heading">Accessibilité</h2>
+							<p class="text-desc why">
+								Toutes <strong class="game">nos formations essentielles</strong> 
+								c'est à dire les formations qui vous permettront de réaliser des <strong class="game">choses valables en cette époque sont gratuites</strong>.
+								Cependant, il existe des <strong class="game">formations premiums qui vous permettent d'élargir vos horizons afin de devenir des experts
+								</strong> et d'affronter les grands défis de ce siècle. Veuillez cliquer <a href="" id="ici">ici</a> 
+								pour voir la liste complète de tous nos cours.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<form action="" method="">
+			<fieldset>
+				<legend>Faire un don?</legend>
+				<div class="F-A">
+					<p class="gift">Pour changer le monde et aider la société nous avons besoin du soutient de tout le monde.</p>
+				<p class="math">
+					<label for="carte_number">Entrez le numéro de la carte: <input type="number" placeholder="3345 1245 1823" pattern="XXX-XXX-XXX" name="carte_number"></label>
+					<label for="amount_card">Entrez le montant: <input type="number" placeholder="0.00" name="amount_card" id="currency"></label>
+					<select id="currency" name="currency">
+					    <option>Sélectionnez une devise</option>
+					    <option value="AFN">Afghan Afghani</option>
+					    <option value="ALL">Albanian Lek</option>
+					    <option value="DZD">Algerian Dinar</option>
+					    <option value="AOA">Angolan Kwanza</option>
+					    <option value="ARS">Argentine Peso</option>
+					    <option value="AMD">Armenian Dram</option>
+					    <option value="AWG">Aruban Florin</option>
+					    <option value="AUD">Australian Dollar</option>
+					    <option value="AZN">Azerbaijani Manat</option>
+					    <option value="BSD">Bahamian Dollar</option>
+					    <option value="BHD">Bahraini Dinar</option>
+					    <option value="BDT">Bangladeshi Taka</option>
+					    <option value="BBD">Barbadian Dollar</option>
+					    <option value="BYR">Belarusian Ruble</option>
+					    <option value="BEF">Belgian Franc</option>
+					    <option value="BZD">Belize Dollar</option>
+					    <option value="BMD">Bermudan Dollar</option>
+					    <option value="BTN">Bhutanese Ngultrum</option>
+					    <option value="BTC">Bitcoin</option>
+					    <option value="BOB">Bolivian Boliviano</option>
+					    <option value="BAM">Bosnia-Herzegovina Convertible Mark</option>
+					    <option value="BWP">Botswanan Pula</option>
+					    <option value="BRL">Brazilian Real</option>
+					    <option value="GBP">British Pound Sterling</option>
+					    <option value="BND">Brunei Dollar</option>
+					    <option value="BGN">Bulgarian Lev</option>
+					    <option value="BIF">Burundian Franc</option>
+					    <option value="KHR">Cambodian Riel</option>
+					    <option value="CAD">Canadian Dollar</option>
+					    <option value="CVE">Cape Verdean Escudo</option>
+					    <option value="KYD">Cayman Islands Dollar</option>
+					    <option value="XOF">CFA Franc BCEAO</option>
+					    <option value="XAF">CFA Franc BEAC</option>
+					    <option value="XPF">CFP Franc</option>
+					    <option value="CLP">Chilean Peso</option>
+					    <option value="CNY">Chinese Yuan</option>
+					    <option value="COP">Colombian Peso</option>
+					    <option value="KMF">Comorian Franc</option>
+					    <option value="CDF">Congolese Franc</option>
+					    <option value="CRC">Costa Rican ColÃ³n</option>
+					    <option value="HRK">Croatian Kuna</option>
+					    <option value="CUC">Cuban Convertible Peso</option>
+					    <option value="CZK">Czech Republic Koruna</option>
+					    <option value="DKK">Danish Krone</option>
+					    <option value="DJF">Djiboutian Franc</option>
+					    <option value="DOP">Dominican Peso</option>
+					    <option value="XCD">East Caribbean Dollar</option>
+					    <option value="EGP">Egyptian Pound</option>
+					    <option value="ERN">Eritrean Nakfa</option>
+					    <option value="EEK">Estonian Kroon</option>
+					    <option value="ETB">Ethiopian Birr</option>
+					    <option value="EUR">Euro</option>
+					    <option value="FKP">Falkland Islands Pound</option>
+					    <option value="FJD">Fijian Dollar</option>
+					    <option value="GMD">Gambian Dalasi</option>
+					    <option value="GEL">Georgian Lari</option>
+					    <option value="DEM">German Mark</option>
+					    <option value="GHS">Ghanaian Cedi</option>
+					    <option value="GIP">Gibraltar Pound</option>
+					    <option value="GRD">Greek Drachma</option>
+					    <option value="GTQ">Guatemalan Quetzal</option>
+					    <option value="GNF">Guinean Franc</option>
+					    <option value="GYD">Guyanaese Dollar</option>
+					    <option value="HTG">Haitian Gourde</option>
+					    <option value="HNL">Honduran Lempira</option>
+					    <option value="HKD">Hong Kong Dollar</option>
+					    <option value="HUF">Hungarian Forint</option>
+					    <option value="ISK">Icelandic KrÃ³na</option>
+					    <option value="INR">Indian Rupee</option>
+					    <option value="IDR">Indonesian Rupiah</option>
+					    <option value="IRR">Iranian Rial</option>
+					    <option value="IQD">Iraqi Dinar</option>
+					    <option value="ILS">Israeli New Sheqel</option>
+					    <option value="ITL">Italian Lira</option>
+					    <option value="JMD">Jamaican Dollar</option>
+					    <option value="JPY">Japanese Yen</option>
+					    <option value="JOD">Jordanian Dinar</option>
+					    <option value="KZT">Kazakhstani Tenge</option>
+					    <option value="KES">Kenyan Shilling</option>
+					    <option value="KWD">Kuwaiti Dinar</option>
+					    <option value="KGS">Kyrgystani Som</option>
+					    <option value="LAK">Laotian Kip</option>
+					    <option value="LVL">Latvian Lats</option>
+					    <option value="LBP">Lebanese Pound</option>
+					    <option value="LSL">Lesotho Loti</option>
+					    <option value="LRD">Liberian Dollar</option>
+					    <option value="LYD">Libyan Dinar</option>
+					    <option value="LTL">Lithuanian Litas</option>
+					    <option value="MOP">Macanese Pataca</option>
+					    <option value="MKD">Macedonian Denar</option>
+					    <option value="MGA">Malagasy Ariary</option>
+					    <option value="MWK">Malawian Kwacha</option>
+					    <option value="MYR">Malaysian Ringgit</option>
+					    <option value="MVR">Maldivian Rufiyaa</option>
+					    <option value="MRO">Mauritanian Ouguiya</option>
+					    <option value="MUR">Mauritian Rupee</option>
+					    <option value="MXN">Mexican Peso</option>
+					    <option value="MDL">Moldovan Leu</option>
+					    <option value="MNT">Mongolian Tugrik</option>
+					    <option value="MAD">Moroccan Dirham</option>
+					    <option value="MZM">Mozambican Metical</option>
+					    <option value="MMK">Myanmar Kyat</option>
+					    <option value="NAD">Namibian Dollar</option>
+					    <option value="NPR">Nepalese Rupee</option>
+					    <option value="ANG">Netherlands Antillean Guilder</option>
+					    <option value="TWD">New Taiwan Dollar</option>
+					    <option value="NZD">New Zealand Dollar</option>
+					    <option value="NIO">Nicaraguan CÃ³rdoba</option>
+					    <option value="NGN">Nigerian Naira</option>
+					    <option value="KPW">North Korean Won</option>
+					    <option value="NOK">Norwegian Krone</option>
+					    <option value="OMR">Omani Rial</option>
+					    <option value="PKR">Pakistani Rupee</option>
+					    <option value="PAB">Panamanian Balboa</option>
+					    <option value="PGK">Papua New Guinean Kina</option>
+					    <option value="PYG">Paraguayan Guarani</option>
+					    <option value="PEN">Peruvian Nuevo Sol</option>
+					    <option value="PHP">Philippine Peso</option>
+					    <option value="PLN">Polish Zloty</option>
+					    <option value="QAR">Qatari Rial</option>
+					    <option value="RON">Romanian Leu</option>
+					    <option value="RUB">Russian Ruble</option>
+					    <option value="RWF">Rwandan Franc</option>
+					    <option value="SVC">Salvadoran ColÃ³n</option>
+					    <option value="WST">Samoan Tala</option>
+					    <option value="SAR">Saudi Riyal</option>
+					    <option value="RSD">Serbian Dinar</option>
+					    <option value="SCR">Seychellois Rupee</option>
+					    <option value="SLL">Sierra Leonean Leone</option>
+					    <option value="SGD">Singapore Dollar</option>
+					    <option value="SKK">Slovak Koruna</option>
+					    <option value="SBD">Solomon Islands Dollar</option>
+					    <option value="SOS">Somali Shilling</option>
+					    <option value="ZAR">South African Rand</option>
+					    <option value="KRW">South Korean Won</option>
+					    <option value="XDR">Special Drawing Rights</option>
+					    <option value="LKR">Sri Lankan Rupee</option>
+					    <option value="SHP">St. Helena Pound</option>
+					    <option value="SDG">Sudanese Pound</option>
+					    <option value="SRD">Surinamese Dollar</option>
+					    <option value="SZL">Swazi Lilangeni</option>
+					    <option value="SEK">Swedish Krona</option>
+					    <option value="CHF">Swiss Franc</option>
+					    <option value="SYP">Syrian Pound</option>
+					    <option value="STD">São Tomé and Príncipe Dobra</option>
+					    <option value="TJS">Tajikistani Somoni</option>
+					    <option value="TZS">Tanzanian Shilling</option>
+					    <option value="THB">Thai Baht</option>
+					    <option value="TOP">Tongan pa'anga</option>
+					    <option value="TTD">Trinidad & Tobago Dollar</option>
+					    <option value="TND">Tunisian Dinar</option>
+					    <option value="TRY">Turkish Lira</option>
+					    <option value="TMT">Turkmenistani Manat</option>
+					    <option value="UGX">Ugandan Shilling</option>
+					    <option value="UAH">Ukrainian Hryvnia</option>
+					    <option value="AED">United Arab Emirates Dirham</option>
+					    <option value="UYU">Uruguayan Peso</option>
+					    <option value="USD">US Dollar</option>
+					    <option value="UZS">Uzbekistan Som</option>
+					    <option value="VUV">Vanuatu Vatu</option>
+					    <option value="VEF">Venezuelan BolÃ­var</option>
+					    <option value="VND">Vietnamese Dong</option>
+					    <option value="YER">Yemeni Rial</option>
+					    <option value="ZMK">Zambian Kwacha</option>
+					</select>
+					<input type="submit" name="submit" value="envoyer" id="submit">
+				</p>
+				</div>
+			</fieldset>
+		</form>
+	</section>
+	<?php include("footer.php"); ?>
+</main>
+</body>
+</html>
